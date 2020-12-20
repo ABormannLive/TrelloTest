d98b0bac4913aaa19bc199482efe6ec78b5c59e4
ABormannLive <Andreas.Bormann@live.de>
2020-12-20 15:13:21 +0100

    Callback and butler tests

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ const options = {
   passphrase: ''
 };
 
+function pad(n, len) {
+    s = n.toString();
+    if (s.length < len) {
+        s = ('0000' + s).slice(-len);
+    }
+    return s;
+}
+
 const app = https.createServer(options, (request, response) => {
   var path = url.parse(request.url).pathname;  
   switch (path) {  
-      case '/':  
+    case '/':  
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write('<h1>Some basic response</h1>');
         response.end();
         break;  
-      case '/index.html':  
+    case '/index.html':  
           fs.readFile('src/html' + path, function(error, data) {  
               if (error) {  
                   response.writeHead(404);  
@@ -24,6 +32,93 @@ const app = https.createServer(options, (request, response) => {
               } else {  
                   response.writeHead(200, {  
                       'Content-Type': 'text/html'  
+                  });  
+                  response.write(data);  
+                  response.end();  
+              }  
+          });  
+          break;  
+          
+    case '/movecard':  
+        var urlQuery = url.parse(request.url).query;
+        // var urlParams = url.parse(request.url).URLSearchParams;
+        var urlParams =  new URLSearchParams(urlQuery);
+        var cardname = urlParams.get('q');
+        var cardlist = urlParams.get('l');
+        var timestamp = new Date();
+        var date = timestamp.getFullYear()+'-' + pad((timestamp.getMonth()+1),2) + '-' + pad(timestamp.getDate(),2) + ' ' + pad(timestamp.getHours(),2) + ':'  + pad(timestamp.getMinutes(),2) + ':' + pad(timestamp.getSeconds(),2);
+        fs.writeFile('data/' + cardname, date,{ encoding: 'utf8' }, function(error) {
+            if (error) {  
+                response.writeHead(404);  
+                response.write(error);  
+                response.end();  
+            } else {  
+                response.writeHead(200, {  
+                    'Content-Type': 'text/html'  
+                });  
+                response.end();  
+            }  
+
+        });
+        break;  
+          
+    case '/cardmove.html':  
+          fs.readFile('src/html' + path, function(error, data) {  
+              if (error) {  
+                  response.writeHead(404);  
+                  response.write(error);  
+                  response.end();  
+              } else {  
+                  response.writeHead(200, {  
+                      'Content-Type': 'text/html'  
+                  });  
+                  response.write(data);  
+                  response.end();  
+              }  
+          });  
+          break;  
+          
+          case '/section.html':  
+          fs.readFile('src/html' + path, function(error, data) {  
+              if (error) {  
+                  response.writeHead(404);  
+                  response.write(error);  
+                  response.end();  
+              } else {  
+                  response.writeHead(200, {  
+                      'Content-Type': 'text/html'  
+                  });  
+                  response.write(data);  
+                  response.end();  
+              }  
+          });  
+          break;  
+          
+          case '/logocmdbuild.png':  
+          fs.readFile('src/html' + path, function(error, data) {  
+              if (error) {  
+                  response.writeHead(404);  
+                  response.write(error);  
+                  response.end();  
+              } else {  
+                  response.writeHead(200, {  
+                      'Content-Type': 'image/png'  
+                  });  
+                  response.write(data);  
+                  response.end();  
+              }  
+          });  
+          break;  
+          
+          case '/core.less':  
+          fs.readFile('src/html' + path, function(error, data) {  
+              if (error) {  
+                  response.writeHead(404);  
+                  response.write(error);  
+                  response.end();  
+              } else {  
+                  response.writeHead(200, {  
+                      'Content-Type': 'text/css'  
                   });  
                   response.write(data);  
                   response.end();  
--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,60 @@
 console.log('Hello World!');
 
+// debugger console.log('Hello World!');
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg'
+
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
     // card badge testing function
-    return [];
-    }
-  });
+    return t.card('all')
+    .then(function(card) {
+      console.log(card);
+      return [{
+        text: card.idShort
+      }];
+    })
+  },
+  'card-detail-badges': function (t, opts) {
+    // card badge testing function
+    return t.card('all')
+    .then(function(card) {
+      console.log(card);
+      return [{
+        text: card.pos.toString()
+      }];
+    })
+  },
+  'card-buttons': function (t, opts) {
+      return [];
+  },
+  'board-buttons': function (t, opts) {
+      return [];
+  },
+  'card-back-section': function(t, opts){
+    return {
+      title: 'CMDBuild Data',
+      icon: "./logocmdbuild.png", // Must be a gray icon, colored icons not allowed.
+      content: {
+        type: 'iframe',
+        url: t.signUrl('./section.html'),
+        height: 230, // Max height is 1500.
+        action: {
+          text: 'My Action',
+          callback: (t) => t.popup({
+            title: 'Some actions',
+            items: [{
+              text: 'Choose Time',
+              callback: function (t, opts) { }
+            }, {
+              text: 'In 1 hour',
+              callback: function (t, opts) {  }
+            }, {
+              text: 'In 2 hours',
+              callback: function (t, opts) {  }
+            }]
+          }),
+        },
+      }
+    };
+  }
+ });
